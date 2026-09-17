f9d10ed33ca1f177e06bc516fb4e5e34b1b5773d
MinSung_Jo <152842047+MinSung0732@users.noreply.github.com>
2026-09-18 02:37:26 +0900

    chore(harmony): refresh late game ui module version

--- games/harmony/campaign-ui.js
+++ games/harmony/campaign-ui.js
@@ -8,7 +8,7 @@ import {
   resolveAct7Route,
 } from "./campaign-progression.js";
 import { routeLabel } from "./late-game-runtime.js";
-import "./late-game-ui.js?v=20260917-1";
+import "./late-game-ui.js?v=20260918-2";
 
 const STYLE_ID = "harmony-campaign-ui-style";
 let scheduled = false;
