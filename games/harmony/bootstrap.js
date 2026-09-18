9e1321efb407847a6c1a9c897020ddb3ab97f805
MinSung_Jo <152842047+MinSung0732@users.noreply.github.com>
2026-09-18 10:36:28 +0900

    chore(harmony): bump set progress UI cache

--- games/harmony/bootstrap.js
+++ games/harmony/bootstrap.js
@@ -268,7 +268,7 @@ function emitCloudStatus(detail) {
 }
 
 async function importGameModules() {
-  await import("./pc-frame-ui.js?v=20260918-1");
+  await import("./pc-frame-ui.js?v=20260918-2");
   await import("./main.js?v=20260918-1");
   await import("./enemy-hp-visual-guard.js?v=20260914-1");
   await import("./combat-floating-text-portal.js?v=20260914-1");
