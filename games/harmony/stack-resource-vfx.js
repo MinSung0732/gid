ab00ba36e2cd0473c83fbc012de09c829962927e
MinSung_Jo <152842047+MinSung0732@users.noreply.github.com>
2026-09-22 11:55:29 +0900

    chore(harmony): refresh stack resource status metadata

--- games/harmony/stack-resource-vfx.js
+++ games/harmony/stack-resource-vfx.js
@@ -1,4 +1,4 @@
-import { STATUS_DEFINITIONS } from "./statuses.js?v=20260922-resonance-1";
+import { STATUS_DEFINITIONS } from "./statuses.js?v=20260922-concentration-1";
 
 function clampParticleCount(delta, reduced) {
   if (reduced) return 1;
